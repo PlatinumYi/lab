3b750089ac39c0eb4e70bc1e2b7da10c5a9d3f18
Howie Lu <stegluhao@163.com>
2019-08-16 10:52:01 +0800

    完成部署前置任务，准备最新版本

--- src/main/java/com/xuenan/lab/StarterApplication.java
+++ src/main/java/com/xuenan/lab/StarterApplication.java
@@ -2,10 +2,16 @@ package com.xuenan.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StarterApplication {
+public class StarterApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StarterApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class, args);
     }
